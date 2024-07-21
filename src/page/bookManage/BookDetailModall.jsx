bed43e3ea5b5a38ba88338f8c2a8a9640f954caf
ndDuke <104844845+ndDuke@users.noreply.github.com>
2024-07-21 09:50:56 +0700

    design & css upload image

--- src/page/bookManage/BookDetailModall.jsx
+++ src/page/bookManage/BookDetailModall.jsx
@@ -1,17 +1,52 @@
 // import {useEffect, useState} from "react";
-import {Drawer,} from 'antd';
+import {Button, Drawer,} from 'antd';
 import {useState} from "react";
 const BookDetailModall=(props)=>{
+    const [selectedFile, setSelectedFile] = useState(null)
+    const [preview, setPreview] = useState(null)
+
     const {dataDetail,
         setDataDetail,
         isDetailOpen,
         setIsDataDetail,}=props
+    const handleOnChangeFile=(e)=>{
+        if (!e.target.files || e.target.files.length === 0) {
+            return
+        }
+        const file=e.target.files[0]
+        if(file){
+            setSelectedFile(file);
+            setPreview(URL.createObjectURL(file))
+        }
+    }
+    const handleUploadAvatar=()=>{
+        console.log(">>check file",selectedFile)
+    }
+
     return(
 
         <>
             <Drawer title="User Detail" open={isDetailOpen} onClose={()=> {setDataDetail(null);setIsDataDetail(false)}}>
                 {dataDetail?
                     <>
+                        <div style={{display: "grid", justifyItems: "center"}}>
+                            <div
+                                style={{margin: "20px 0", height: "150px", width: "180px", border: "2px solid e9ecef"}}>
+                                <img style={{height: "100%", width: "100%",}}
+                                     src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail.avatar}`}/>
+                            </div>
+                            <div>
+                                <label style={{
+                                    display: "block",
+                                    backgroundColor: "gold",
+                                    padding: " 10px 30px",
+                                    borderRadius: "10px"
+                                }} htmlFor="bthUploadFile">Upload File</label>
+                                <input type="file" hidden id='bthUploadFile'
+                                       onChange={(event) => handleOnChangeFile(event)}
+                                />
+                            </div>
+                        </div>
                         <div style={{margin: "20px 0"}}>
                             <p>Full name: {dataDetail.fullName}</p>
                         </div>
@@ -24,7 +59,21 @@ const BookDetailModall=(props)=>{
                         <div style={{margin: "20px 0"}}>
                             <p>Phone number: {dataDetail.phone}</p>
                         </div>
-                </>:<p>Không có dữ liệu</p>
+                        {preview && <>
+                            <div
+
+                                style={{margin: "20px 0", height: "100px", width: "120px", border: "2px solid e9ecef"}}>
+                                <p>Preview</p>
+
+                                <img style={{height: "100%", width: "100%",}}
+                                     src={preview}/>
+                                <Button type="primary"
+                                    onClick={()=>handleUploadAvatar()}
+                                >Save</Button>
+                            </div>
+
+                        </>}
+                    </> : <p>Không có dữ liệu</p>
                 }
             </Drawer>
         </>
